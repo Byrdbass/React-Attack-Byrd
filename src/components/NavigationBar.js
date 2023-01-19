418507260b8633b6772a7833766e9b7238f783fc
ByrdBassPC <byrdbass@gmail.com>
2023-01-19 10:18:04 -0600

    made dropdown appear before navItems, added Icons, but width is a little too skinny

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,7 +7,7 @@ import Resume from './pages/Resume'
 import ContactFor from './pages/ContactForm';
 import Aboutme from './pages/Aboutme';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Navbar, NavItem, Tabs, Tab, Dropdown, Icon, Divider, Button } from 'react-materialize';
+import { Navbar, NavItem, Tabs, Tab, Dropdown, Icon, Divider, Button, Col, Row } from 'react-materialize';
 import ContactForm from './pages/ContactForm';
 
 
@@ -71,7 +71,7 @@ function NavigationBar() {
                                 }}
                                 trigger={<Button node="button">Full Stack Projects</Button>}
                             >
-                               <Portfolio/>
+                                <Portfolio />
                                 <a href="#">
                                     two
                                 </a>
@@ -124,6 +124,45 @@ function NavigationBar() {
                         <li><a href="https://linktr.ee/byrdbass" target={"_blank"}>Linktr.ee</a></li>
                     </ul>}
                 >
+                    <Dropdown
+                        id="Dropdown_8"
+                        options={{
+                            alignment: 'center',
+                            autoTrigger: true,
+                            closeOnClick: true,
+                            constrainWidth: true,
+                            container: null,
+                            coverTrigger: true,
+                            hover: true,
+                            inDuration: 150,
+                            onCloseEnd: null,
+                            onCloseStart: null,
+                            onOpenEnd: null,
+                            onOpenStart: 250,
+                            outDuration: 250
+                        }}
+                        trigger={<a>Projects<Icon right>arrow_drop_down_circle</Icon></a>}
+                    >
+                        <a><Icon>apps</Icon> Full stack</a>
+                        <a href="#">Java</a>
+                        <a href="#">Node</a>
+                        <a>Python</a>
+                        <a><Icon>art_track</Icon>FrontEnd</a>
+                        <a href="#">
+                            <Icon>
+                                view_module
+                            </Icon>
+                            four
+                        </a>
+                        {/* Keeping this as an example */}
+                        {/* <a href="#">
+                                    <Icon>
+                                        cloud
+                                    </Icon>
+                                    {<Portfolio/> }five
+                                </a> */}
+                    </Dropdown>
+
 
                     <NavItem href="https://github.com/Byrdbass" target={"_blank"}> Github </NavItem>
                     <NavItem href="https://www.linkedin.com/in/leland-byrd/" target={"_blank"}>LinkedIn</NavItem>
